668e844b19df5b3eea32ddf6bbc2f1739a7161a8
Yasir-Khan-7 <yasirkhandps7@gmail.com>
2024-07-20 01:26:17 +0500

    added plotting feature

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import csv
 from datetime import datetime
+import matplotlib.pyplot as plt
 from data_entry import get_amount,get_category,get_date,get_description
+
 class CSV:
     CSV_FILE ='finance_data.csv'
     COLUMNS  = ['date','amount','category','description']
@@ -25,13 +27,8 @@ class CSV:
             writer = csv.DictWriter(csvfile,fieldnames=cls.COLUMNS)     
             writer.writerow(new_entry)  
             print("Entry added Successfully")
-    def add():
-        CSV.initialize_csv()
-        date = get_date("Enter the date of Transaction (dd--mm-yyyy) or enter for today's date: ")
-        amount =get_amount()
-        category = get_category()
-        description = get_description()
-        CSV.add_entry(date,amount,category,description)
+            
+          
     @classmethod
     def get_transaction(cls,start_date,end_date):
         df = pd.read_csv(cls.CSV_FILE)
@@ -53,13 +50,50 @@ class CSV:
             print(f"Net Savings ${(total_income-total_expense):.2f}")
             
         return filtered_df
-    def main():
-        while True:
-            print("\n 1. add new Transaction")
-            print("2. View Summary and Transaction within a date range")   
-            print("3. Exit ")
-            choice = input("Enter your choice  (1-3): ")
-            
+ 
+def plot_Transaction(df):
+    df.set_index("date",inplace= True)
+    income_df = df[df["category"]=="Income"].resample("D").sum().reindex(df.index,fill_value =0)
+    expense_df = df[df["category"]=="Expense"].resample("D").sum().reindex(df.index,fill_value =0)      
     
-CSV.get_transaction("1-01-2023","30-07-2024")
-CSV.add()
+    plt.figure(figsize=(10,5)) 
+    plt.plot(income_df.index, income_df["amount"],label="Income",color="g")   
+    plt.plot(expense_df.index, expense_df["amount"],label="Expense",color="r") 
+    
+    plt.xlabel("date")  
+    plt.ylabel("amount")
+    plt.title("Income and Expense over Time")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+    
+def add():
+    CSV.initialize_csv()
+    date = get_date("Enter the date of Transaction (dd--mm-yyyy) or enter for today's date: ")
+    amount =get_amount()
+    category = get_category()
+    description = get_description()
+    CSV.add_entry(date,amount,category,description)
+  
+def main():
+    while True:
+        print("\n 1. add new Transaction")
+        print("2. View Summary and Transaction within a date range")   
+        print("3. Exit ")
+        choice = input("Enter your choice  (1-3): ")
+
+        if choice == "1":
+            add()
+        elif choice =="2":
+            start_date = get_date("enter the start date (dd-mm-YYYY)")
+            end_date = get_date("enter the end date (dd-mm-YYYY)")
+            df =CSV.get_transaction(start_date,end_date)
+            if input("do you want to plot (y/n): ").lower()=='y':
+                plot_Transaction(df)
+        elif choice =="3":
+            print("Exiting...")
+            break
+        else:
+            print("Invalid choice Enter 1,2 or 3")
+if __name__ == "__main__":
+    main()
